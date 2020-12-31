935fd686a5660912f78f4cd4e976ae4cffde3ec0
Joerg Barkoczi <34109209+jabra98@users.noreply.github.com>
2020-12-31 03:32:11 +0100

    add files

--- src/1.cpp
+++ src/1.cpp
@@ -4,7 +4,20 @@
 using i64 = std::int64_t;
 using u64 = std::uint64_t;
 
+std::vector<std::string> get_input()
+{
+    auto file = std::ifstream("../etc/data.txt");
+
+    auto res = std::vector<std::string>();
+
+    std::string tmp;
+    while (std::getline(file, tmp))
+    {
+        res.push_back(tmp);
+    }
+    return res;
+}
+
 int main()
 {
-    std::cout << "readytogo\n";     
 }
