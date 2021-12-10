e0257d7c95448b6b2be77152a26993190856ae66
jabra98 <jabra98.gpg@gmail.com>
2021-12-10 20:50:09 +0100

    day10

---
+++ archieve/2021/day10a.py
@@ -0,0 +1,23 @@
+lines = open('etc/in').read().splitlines()
+
+points = {')':3, ']':57, '}':1197, '>':25137}
+
+pairs = {'(':')','<':'>','[':']','{':'}',
+         ')':'(','>':'<',']':'[','}':'{'}
+
+ans = 0
+for i in lines:
+    s = list()
+    for j in i:
+        if j in ['(','<','[','{']:
+            s.append(j)
+        else:
+            if len(s) == 0:
+                continue
+            top = s[len(s)-1]
+            s.pop()
+            
+            if j != pairs[top]:
+                ans += points[j]
+                break
+print(ans)
---
+++ archieve/2021/day10b.py
@@ -0,0 +1,41 @@
+lines = open('etc/in').read().splitlines()
+
+pairs = {'(':')','<':'>','[':']','{':'}',
+         ')':'(','>':'<',']':'[','}':'{'}
+
+end_seqs = list()
+ans = 0
+for i in lines:
+    s = list()
+    is_valid=True
+    for j in i:
+        if j in ['(','<','[','{']:
+            s.append(j)
+        else:
+            if len(s) == 0:
+                continue
+            top = s[len(s)-1]
+            s.pop()
+            
+            if j != pairs[top]:
+                is_valid = False
+                break
+    if is_valid:
+        end_seqs.append(s)
+
+points = {')':1, ']':2, '}':3, '>':4}
+
+p = list()
+for i in end_seqs:
+    if len(i) == 0:
+        continue
+    anw = 0
+    while len(i) > 0:
+        top = i[len(i)-1]
+        i.pop()
+        anw *= 5
+        anw += points[pairs[top]]
+    p.append(anw)
+
+p = sorted(p)
+print(p[int(len(p)/2)])
