43e2a41de8b7498a65b0a52859be623ebd5d79d7
jabra98 <jabra98.gpg@gmail.com>
2021-12-14 18:18:34 +0100

    day13
    part2 is a hack, will probably only work for my input .. need to clean
    this later

---
+++ archieve/2021/day13a.py
@@ -0,0 +1,53 @@
+data = [x for x in open('etc/in').read().split('\n\n')]
+
+points = [ x.split(',') for x in data[0].splitlines()]
+for i in range(len(points)):
+    a,b = points[i]
+    points[i] = [int(a), int(b)]
+
+folds = list()
+for i in data[1].splitlines():
+    if 'x' in i:
+        folds.append(['x', int(i[13:])])
+    else:
+        folds.append(['y', int(i[13:])])
+
+grid = list(list())
+
+min_x = min([x for x,_ in points])
+min_y = min([y for _,y in points])
+max_x = max([x for x,_ in points])
+max_y = max([y for _,y in points])
+
+for i in range(min_y,max_y+1):
+    grid.append([0] * (max_x-min_x+1))
+
+for (x,y) in points:
+    grid[y-min_y][x-min_x] = 1
+
+for (coord, idx) in folds[0:1]:
+    if coord == 'y':
+        tmp = grid[0:idx].copy()
+
+        for i in range(len(tmp)):
+            for j in range(len(tmp[i])):
+                if tmp[i][j] == 0:
+                    tmp[i][j] = grid[len(grid)-1-i][j]
+        grid = tmp
+    else:
+        tmp = list(list())
+        for i in range(len(grid)):
+            tmp.append(grid[i][0:idx].copy())
+
+        for i in range(len(tmp)):
+            for j in range(len(tmp[i])):
+                if tmp[i][j] == 0:
+                    tmp[i][j] = grid[i][len(grid[i])-j-1]
+        grid = tmp
+
+ctr=0
+for i in grid:
+    for j in i:
+        if j == 1:
+            ctr+=1
+print(ctr)
---
+++ archieve/2021/day13b.py
@@ -0,0 +1,68 @@
+data = [x for x in open('etc/in').read().split('\n\n')]
+
+points = [ x.split(',') for x in data[0].splitlines()]
+for i in range(len(points)):
+    a,b = points[i]
+    points[i] = [int(a), int(b)]
+
+
+grid = list(list())
+
+min_x = min([x for x,_ in points])
+min_y = min([y for _,y in points])
+max_x = max([x for x,_ in points])
+max_y = max([y for _,y in points])
+
+if max_x % 2 != 0:
+    max_x+=1
+
+if max_y % 2 != 0:
+    max_y+=1
+
+
+for i in range(0,max_y+3):
+    grid.append([0] * (max_x+1))
+
+for (x,y) in points:
+    grid[y][x] = 1
+
+folds = list()
+for i in data[1].splitlines():
+    if 'x' in i:
+        folds.append(['x', int(i[13:])])
+    else:
+        folds.append(['y', int(i[13:])])
+
+
+
+def print_g():
+    for i in grid:
+        for j in i:
+            if j == 1:
+                print('#',end='')
+            else:
+                print('.',end='')
+        print()
+    print()
+
+for (coord, idx) in folds:
+    if coord == 'y':
+        tmp = grid[0:idx].copy()
+
+        for i in range(len(tmp)):
+            for j in range(len(tmp[i])):
+                if tmp[i][j] == 0:
+                    tmp[i][j] = grid[len(grid)-1-i][j]
+        grid = tmp
+    else:
+
+        tmp = list(list())
+        for i in range(len(grid)):
+            tmp.append(grid[i][0:idx].copy())
+
+        for i in range(len(tmp)):
+            for j in range(len(tmp[i])):
+                if tmp[i][j] == 0:
+                    tmp[i][j] = grid[i][len(grid[i])-j-1]
+        grid = tmp
+print_g()
