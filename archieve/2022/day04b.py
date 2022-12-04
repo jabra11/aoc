587bc46b8bcd9fbdb47d3d33032456e3d48c2946
jabra11 <jabra11.gpg@gmail.com>
2022-12-04 15:10:50 +0100

    day4!

---
+++ archieve/2022/day04b.py
@@ -0,0 +1,7 @@
+import sys; datafilepath = sys.argv[1]
+data = open(datafilepath).read().splitlines()
+
+ap = [list(map(lambda y : int(y), y.split('-'))) for x in data for y in x.split(',')] 
+ap = [ ap[i:i+2] for i in range(0,len(ap),2) ]
+ap = [ a<=c<=b or a<=d<=b or c<=a<=d or c<=b<=d for ((a,b),(c,d)) in ap]
+print(sum(ap))
