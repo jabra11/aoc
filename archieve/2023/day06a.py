84e5902d81ced60bb2012aef811dd08f965f1588
jabra11 <jabra11.gpg@gmail.com>
2023-12-09 14:34:28 +0100

    day6

---
+++ archieve/2023/day06a.py
@@ -0,0 +1,23 @@
+import sys; datafilepath = sys.argv[1]
+data = open(datafilepath).read().splitlines()
+
+times = data[0].split(':')[1]
+dis = data[1].split(':')[1]
+
+times = list(filter(lambda x: x != '', times.split(' ')))
+times = [int(x) for x in times]
+dis = list(filter(lambda x: x != '', dis.split(' ')))
+dis = [int(x) for x in dis]
+
+records = list(zip(times,dis))
+
+answ = 1
+for time,dis in records:
+    ctr = 0
+    for hold in range(0,time):
+        ndis = (time-hold)*hold
+        if ndis>dis:
+            ctr+=1
+    answ *= ctr
+
+print(answ)
